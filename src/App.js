8e05db1cd093e152a926fa3034bd6743e73cf092
cordenbrock <cordenbrock@gmail.com>
2021-09-22 14:51:39 -0700

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import ImageCard from './components/ImageCard';
 
 function App() {
+const [images, setImages] = useState([]);
+const [isLoading, setLoading] = useState(true);
+const [term, setTerm] = useState('');
+
+useEffect(() => {
+  fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_API_KEY}&q=${term}&image_type=photo`)
+    .then(res => res.json())
+    .then(data => {
+      setImages(data.hits)
+      setLoading(false)
+    })
+    .catch(err => console.log(err));
+}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+        {images.map((image, index) => (
+          <ImageCard key={index} image={image} />
+        ))}H
+      </div>
     </div>
   );
 }
